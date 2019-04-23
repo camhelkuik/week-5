20e5b2094153cbf5de0e121a3dd4a066ab1c770a
Cameron Helkuik <cameron@travefy.com>
2019-04-23 10:41:39 -0500

    Initial commit

---
+++ Assignment5/Assignment5/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Assignment5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblDateTime = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(213, 72);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(398, 68);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "&Generate Random Senteces";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(397, 800);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(184, 48);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(587, 800);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(184, 48);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblDateTime
+            // 
+            this.lblDateTime.AutoSize = true;
+            this.lblDateTime.Location = new System.Drawing.Point(86, 702);
+            this.lblDateTime.Name = "lblDateTime";
+            this.lblDateTime.Size = new System.Drawing.Size(126, 25);
+            this.lblDateTime.TabIndex = 3;
+            this.lblDateTime.Text = "lblDateTime";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(91, 180);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(679, 508);
+            this.txtOutput.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 907);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.lblDateTime);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblDateTime;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ Assignment5/Assignment5/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Assignment5
+{
+    public partial class Form1 : Form
+    {
+        string[] article = { "the", "a", "one", "some", "any" };
+        string[] noun = {"boy", "girl", "dog", "town", "cat", "bat"};
+        string[] verb = { "drove", "jumped", "ran", "walked", "skipped", "flew" };
+        string[] preposition = { "to", "from", "over", "under", "on", "around" };
+
+        string title = "Sentence Generator";
+        string myName = " Cameron Helkuik";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text = string.Concat(title, myName);
+            lblDateTime.Text = DateTime.Now.ToLongDateString();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            var output = new StringBuilder();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var generatedString = "";
+                Random rando = new Random();
+
+                var firstWord = article[rando.Next(0, article.Length)];
+                var uppercase =  char.ToUpper(firstWord[0]) + firstWord.Substring(1);
+
+                generatedString += uppercase + " ";
+                generatedString += noun[rando.Next(0, noun.Length)] + " ";
+                generatedString += verb[rando.Next(0, verb.Length)] + " ";
+                generatedString += preposition[rando.Next(0, preposition.Length)] + " ";
+                generatedString += article[rando.Next(0, article.Length)] + " ";
+                generatedString += noun[rando.Next(0, noun.Length)] + ". ";
+
+                output.Append(generatedString);
+
+                //For some reason if I don't have this Thread.Sleep(20), it prints the same sentence to the txtOutput. When I try to debug it with 
+                //a breakpoint, it would work correctly and give me a new sentence. 
+                Thread.Sleep(20);
+            }
+
+            txtOutput.Text = output.ToString();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtOutput.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
